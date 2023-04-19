4e54a1283f62beea8c2baa3acb6fffacf5af9675
GP6A@ <gp6a@GP6As-MacBook-Air.local>
2023-04-20 00:39:15 +0530

    starting new blog app

---
+++ blog-app/src/test/java/com/project/app/blogapp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.app.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
